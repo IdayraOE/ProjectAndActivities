109672d54991070445c3260671dd580ecaf039e5
IdayraOE <idayraores@gmail.com>
2018-02-23 14:52:51 +0100

    activities modificated

---
+++ DAW/Proyecto/wordsKids.js
@@ -0,0 +1,13 @@
+function prueba(){
+ console.log(document.getElementById('time').checked);
+ console.log(document.getElementById('rotate').checked);
+}
+
+function inicio(time, rotate){
+	if (time==true) {
+
+	}
+	if (rotate==true) {
+		
+	}
+}
